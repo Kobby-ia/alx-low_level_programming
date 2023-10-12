e67c13b74c4c5984e253a39b47c5066978b4406c
Israel Aidoo <israel.aidoo@yahoo.com>
2023-10-11 18:45:03 -0700

    puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Calls puts and receives values by puts execution.
+ * Always return 0 to indicate success
+ */
 int main(void)
 {
-	puts("\"Programming is like buil        ding a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
